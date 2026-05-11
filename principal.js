7c797bac27b40b68deb3847cea591354088b9838
Daniela Carolina Ramirez Agudo <danicarolinag@gmail.com>
2026-05-11 10:46:56 -0400

    ready

--- principal.js
+++ principal.js
@@ -5,10 +5,9 @@ function cambiarCantidad(tipo, cambio) {
 
     cantidad += cambio;
     
-    const minimo = (tipo === 'adults') ? 1 : 0;
-    if (cantidad < minimo) {
+    const minimo = 0;
+    if (cantidad < minimo) cantidad = minimo;
         cantidad = minimo;
-    }
     
     if (cantidad > 9) {
         cantidad = 9;
@@ -20,15 +19,21 @@ function cambiarCantidad(tipo, cambio) {
 function cambiarCantidad(tipo, cambio) {
     const elemento = document.getElementById('num-' + tipo);
     let cantidad = parseInt(elemento.innerHTML);
-    
+
     cantidad += cambio;
+
+    const minimo = 0;
+
+    if (cantidad < minimo) {
+        cantidad = minimo;
+    }
     
-    const minimo = (tipo === 'adults') ? 1 : 0;
-    if (cantidad < minimo) cantidad = minimo;
-    if (cantidad > 9) cantidad = 9;
+    if (cantidad > 9) {
+        cantidad = 9;
+    }
 
     elemento.innerHTML = cantidad;
-    
+
     actualizarCajaPrincipal();
 }
 
@@ -38,14 +43,20 @@ function actualizarCajaPrincipal() {
     const seniors = parseInt(document.getElementById('num-seniors').innerHTML);
 
     const total = adultos + ninos + seniors;
-    
+
     let textoResumen = "";
-    
-    if (total === 1 && adultos === 1) {
+
+    if (total === 0) {
+        textoResumen = "Select Passengers";
+    } else if (total === 1 && adultos === 1) {
         textoResumen = "1 Adult";
+    } else if (total === 1 && ninos === 1) {
+        textoResumen = "1 Child";
+    } else if (total === 1 && seniors === 1) {
+        textoResumen = "1 Senior";
     } else {
         textoResumen = total + " Passengers";
     }
-    
+
     document.getElementById('passengers-text').innerHTML = textoResumen + ' ▼';
 }
